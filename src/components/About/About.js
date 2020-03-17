befc197b7f3756528a09903710414ddac5eb85f4
AwrenNuit <a.rennaker@yahoo.com>
2020-03-17 14:55:01 -0500

    added rel to github link

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -22,7 +22,7 @@ export default function About() {
       <p>
         I'm also in the market for a job, so if you enjoy this and you or someone you know is hiring software 
         engineers please reach out! All of the code for my projects, including this one, is available to view 
-        on <a href="https://github.com/AwrenNuit" target="_blank">GitHub</a>.
+        on <a href="https://github.com/AwrenNuit" target="_blank" rel="noopener noreferrer">GitHub</a>.
       </p>
     </>
   );
