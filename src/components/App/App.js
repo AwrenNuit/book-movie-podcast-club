9acfc080c8ec381df22388a939f6aa35db27cbf1
AwrenNuit <a.rennaker@yahoo.com>
2020-03-17 18:29:48 -0500

    created router routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import './App.css';
 import About from '../About/About';
 import Home from '../Home/Home';
+import Books from '../Books/Books';
+import Movies from '../Movies/Movies';
+// import Podcasts from '../Podcasts/Podcasts';
 
 export default function App() {
 
   return (
     <>
-      <Home />
-      <About />
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/books" component={Books} />
+        <Route exact path="/movies" component={Movies} />
+        {/* <Route exact path="/podcasts" component={Podcasts} /> */}
+        <Redirect path="/*" render={()=><Redirect to="/" />} />
+      </Router>
     </>
   );
 }
