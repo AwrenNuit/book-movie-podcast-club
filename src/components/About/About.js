dd4ba8b21c801096100e6f5cb739995cbab777d2
AwrenNuit <a.rennaker@yahoo.com>
2020-03-17 11:04:31 -0500

    created template

---
+++ src/components/About/About.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function About() {
+
+  return(
+    <>
+      <p>
+        Hey everyone! I'm Awren. I figure since we're all quarantined for at least a couple weeks we should 
+        dive deeper into how technology can build community. What are your favorite books and movies? Share 
+        them! See what other people are suggesting and check those out! Get your friends and family to use 
+        this and have some discussions about them!
+      </p>
+      <p>
+        I plan on keeping this registration-free. This is an ongoing project where I'm updating things as I 
+        go, so bear with me if some bugs start cropping up. If you have suggestions or run into bugs I'd love 
+        to hear about it! You can email me at 
+        <a href="mailto:awren.nuit@gmail.com?Subject=Book%20and%20Movie%20Club" target="_top">
+          awren.nuit@gmail.com
+        </a>
+        .
+      </p>
+    </>
+  );
+}
