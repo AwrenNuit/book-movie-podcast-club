92b556c5b6ecf2d611f45477c27ef38a8f0d5f44
AwrenNuit <a.rennaker@yahoo.com>
2020-03-17 15:25:31 -0500

    added redux to dependencies

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { db } from '../../firebase';
 
 export default function Books() {
+
+  const bookList = useSelector(state => state.books);
+
+  useEffect(()=>{
+    db.ref(`/books`).on(`value`, snap => {
+      snap.forEach(child => {
+        dispatch({type: `SET_BOOK_LIST`, payload: child.val()});
+      });
+    });
+  }, []);
 
   return(
     <>
       <h1>Books</h1>
+      {JSON.stringify(bookList)}
       <table>
         <thead>
           <tr>
@@ -13,14 +25,13 @@ export default function Books() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>title1</td>
-            <td>author1</td>
-          </tr>
-          <tr>
-            <td>title2</td>
-            <td>author2</td>
-          </tr>
+          {/* {bookList && bookList.map((book, i)=>
+            <tr key={i}>
+              <td>{book.title}</td>
+              <td>{book.author}</td>
+            </tr>
+          )} */}
+          {bookList.title}
         </tbody>
       </table>
     </>
