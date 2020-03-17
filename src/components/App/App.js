3097a01d0adcbabbeaad2394c19e992484b1023c
AwrenNuit <a.rennaker@yahoo.com>
2020-03-17 15:48:43 -0500

    removed test code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './App.css';
-import { db } from '../../firebase';
 import About from '../About/About';
 import Home from '../Home/Home';
 
 export default function App() {
-
-  useEffect(()=>{
-    db.ref(`/books`).on(`value`, snap => {
-      snap.forEach(child => {
-        console.log(child.val());
-      });
-    });
-  }, []);
 
   return (
     <>
