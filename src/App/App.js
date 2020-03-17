7f36d84f07464de2b56f7891bc53918ffab2f8df
AwrenNuit <a.rennaker@yahoo.com>
2020-03-17 10:45:54 -0500

    set up firebase

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import { db } from '../firebase';
 
 export default function App() {
+
+  useEffect(()=>{
+    db.ref(`/books`).on(`value`, snap => {
+      snap.forEach(child => {
+        console.log(child.val());
+      });
+    });
+  }, []);
+
   return (
     <>
       <p>operational</p>
