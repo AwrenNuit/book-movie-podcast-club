1943f18896a0f8a7f585a69a3089b448352425ff
AwrenNuit <a.rennaker@yahoo.com>
2020-03-17 15:28:28 -0500

    initial reducer setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App/App';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const bookListReducer = (state={}, action) => action.type === `SET_BOOK_LIST` ? [...state, action.payload] : state;
+
+const store = createStore(
+  combineReducers({
+  bookListReducer
+  })
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
