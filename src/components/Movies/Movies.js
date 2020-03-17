f48fa86d9c0b647cb33c08b17cbe1133c610021d
AwrenNuit <a.rennaker@yahoo.com>
2020-03-17 15:55:56 -0500

    set up skeleton for adding new movie

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { db } from '../../firebase';
 
@@ -6,6 +6,8 @@ export default function Movies() {
 
   const dispatch = useDispatch();
   const movieList = useSelector(state => state.movieListReducer);
+  const [newTitle, setNewTitle] = useState('');
+  const [newGenre, setNewGenre] = useState('');
 
   useEffect(()=>{
     db.ref(`/movies`).on(`value`, snap => {
@@ -15,9 +17,31 @@ export default function Movies() {
     });
   }, []);
 
+  const addNewMovie = () => {
+
+  }
+
   return(
     <>
       <h1>Movies</h1>
+      <div>
+        <p>Add Book</p>
+        <form onSubmit={addNewMovie}>
+          <input 
+            type="text" 
+            value={newTitle} 
+            onChange={(e)=>setNewTitle(e.target.value)} 
+            placeholder="movie title" 
+          />
+          <input 
+            type="text" 
+            value={newGenre} 
+            onChange={(e)=>setNewGenre(e.target.value)} 
+            placeholder="movie genre" 
+          />
+          <button type="submit">Add to list!</button>
+        </form>
+      </div>
       <table>
         <thead>
           <tr>
