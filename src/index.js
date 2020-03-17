cd6b8acfca2bb41a9328a2ea2c030b0a9969e750
AwrenNuit <a.rennaker@yahoo.com>
2020-03-17 15:31:05 -0500

    updated reducer state from object to array

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import App from './components/App/App';
 
-const bookListReducer = (state={}, action) => action.type === `SET_BOOK_LIST` ? [...state, action.payload] : state;
+const bookListReducer = (state=[], action) => action.type === `SET_BOOK_LIST` ? [...state, action.payload] : state;
 
 const store = createStore(
   combineReducers({
