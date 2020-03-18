e418892287a75e71cf3e85498c05181b3518b790
AwrenNuit <a.rennaker@yahoo.com>
2020-03-17 19:50:51 -0500

    minor language changes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,14 +9,14 @@ export default function About() {
       <div className="about-container">
         <p className="about-p">
           Hey everyone! I'm Awren. I figure since we're all quarantined for at least a couple weeks we should 
-          dive deeper into how technology can build community. What are your favorite books and movies? Share 
-          them! See what other people are suggesting and check those out! Get your friends and family to use 
-          this and have some discussions about them!
+          dive deeper into how technology can build community. What are your favorite books, movies, and 
+          podcasts? Share them! See what other people are suggesting and check those out! Get your friends 
+          and family to use this and have some discussions about them!
         </p>
         <p className="about-p">
-          I plan on keeping this registration-free. This is an ongoing project where I'm updating things as I 
-          go, so bear with me if some bugs start cropping up. If you have suggestions or run into bugs I'd love 
-          to hear about it! You can email me at&nbsp;
+          I plan on keeping this account-free and registration-free. This is an ongoing project where I'm 
+          updating things as I go, so bear with me if some bugs start cropping up. If you have suggestions 
+          or run into bugs I'd love to hear about it! You can email me at&nbsp;
           <a href="mailto:awren.nuit@gmail.com?Subject=Book%20and%20Movie%20Club" target="_top">
             awren.nuit@gmail.com
           </a>
