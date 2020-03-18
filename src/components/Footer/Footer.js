9a2d62d5826cb32242c22566445cf9e6ca18130c
AwrenNuit <a.rennaker@yahoo.com>
2020-03-17 19:41:33 -0500

    created template

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,6 +4,10 @@ export default function Footer() {
 
   return(
     <>
+      <div className="whitespace"></div>
+      <div className="footer-container">
+        <p>&copy; {new Date().getFullYear()} Awren Nuit</p>
+      </div>
     </>
   );
 }
