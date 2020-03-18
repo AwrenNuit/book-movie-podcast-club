4606472b098cddf8b4b699c7675edaef7acdea0d
AwrenNuit <a.rennaker@yahoo.com>
2020-03-17 19:13:55 -0500

    added basic styling and background

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,29 +1,33 @@
 import React from 'react';
+import './About.css';
 
 export default function About() {
 
   return(
-    <>
-      <p>
-        Hey everyone! I'm Awren. I figure since we're all quarantined for at least a couple weeks we should 
-        dive deeper into how technology can build community. What are your favorite books and movies? Share 
-        them! See what other people are suggesting and check those out! Get your friends and family to use 
-        this and have some discussions about them!
-      </p>
-      <p>
-        I plan on keeping this registration-free. This is an ongoing project where I'm updating things as I 
-        go, so bear with me if some bugs start cropping up. If you have suggestions or run into bugs I'd love 
-        to hear about it! You can email me at&nbsp;
-        <a href="mailto:awren.nuit@gmail.com?Subject=Book%20and%20Movie%20Club" target="_top">
-          awren.nuit@gmail.com
-        </a>
-        .
-      </p>
-      <p>
-        I'm also in the market for a job, so if you enjoy this and you or someone you know is hiring software 
-        engineers please reach out! All of the code for my projects, including this one, is available to view 
-        on <a href="https://github.com/AwrenNuit" target="_blank" rel="noopener noreferrer">GitHub</a>.
-      </p>
-    </>
+    <div className="about-main-container">
+      <h1>About</h1>
+      <div className="about-container">
+        <p className="about-p">
+          Hey everyone! I'm Awren. I figure since we're all quarantined for at least a couple weeks we should 
+          dive deeper into how technology can build community. What are your favorite books and movies? Share 
+          them! See what other people are suggesting and check those out! Get your friends and family to use 
+          this and have some discussions about them!
+        </p>
+        <p className="about-p">
+          I plan on keeping this registration-free. This is an ongoing project where I'm updating things as I 
+          go, so bear with me if some bugs start cropping up. If you have suggestions or run into bugs I'd love 
+          to hear about it! You can email me at&nbsp;
+          <a href="mailto:awren.nuit@gmail.com?Subject=Book%20and%20Movie%20Club" target="_top">
+            awren.nuit@gmail.com
+          </a>
+          .
+        </p>
+        <p className="about-p">
+          I'm also in the market for a job, so if you enjoy this and you or someone you know is hiring software 
+          engineers please reach out! All of the code for my projects, including this one, is available to view 
+          on <a href="https://github.com/AwrenNuit" target="_blank" rel="noopener noreferrer">GitHub</a>.
+        </p>
+      </div>
+    </div>
   );
 }
