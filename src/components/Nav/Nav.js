855aa64a212be4b05c8e67c98f294d4fefbfe060
AwrenNuit <a.rennaker@yahoo.com>
2020-03-17 19:02:44 -0500

    created stylesheet, achieved basic navbar styling

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Nav.css';
 
 export default function Nav() {
 
   return(
+    <>
     <nav>
-    <ul>
-      <Link to="/">
-        <li>Home</li>
-      </Link>
-      <Link to="/books">
-        <li>Books</li>
-      </Link>
-      <Link to="/movies">
-        <li>Movies</li>
-      </Link>
-      <Link to="/podcasts">
-        <li>Podcasts</li>
-      </Link>
-      <Link to="/about">
-        <li>About</li>
-      </Link>
-    </ul>
+      <ul>
+        <Link to="/">
+          <li className="nav-name">Cozy Community Collective</li>
+        </Link>
+        <Link to="/">
+          <li>Home</li>
+        </Link>
+        <Link to="/books">
+          <li>Books</li>
+        </Link>
+        <Link to="/movies">
+          <li>Movies</li>
+        </Link>
+        <Link to="/podcasts">
+          <li>Podcasts</li>
+        </Link>
+        <Link to="/about">
+          <li>About</li>
+        </Link>
+      </ul>
     </nav>
+    </>
   );
 }
