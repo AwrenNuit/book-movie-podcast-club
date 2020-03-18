fc35aa309c1dc9c0a61f679ba92470f70f915e8d
AwrenNuit <a.rennaker@yahoo.com>
2020-03-17 19:58:07 -0500

    reworked component to be an introduction of sorts

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import Books from '../Books/Books';
-import Movies from '../Movies/Movies';
-import Podcasts from '../Podcasts/Podcasts';
 
 export default function Home() {
 
   return(
-    <>
+    <div>
       <div>
-        <Books />
+        <p>
+          Hello and welcome to the Cozy Community Collective! This is a place to share some of your 
+          favorite books, movies, and podcasts. What are some feel-good movies that boost your mood? 
+          How about some books that make all the problems of the real-world seem far, far away? Got a 
+          great podcast? Maybe some media to help us learn new skills? Awesome cooking resources? 
+          Share them all!
+        </p>
+        <p>
+          I aim to keep this account-free and totally open to the public, so please don't abuse this! I 
+          would prefer to keep things nice and open. That said, if there are any suggestions on stuff 
+          that might make this better let me know. Email me at 
+          <a href="mailto:awren.nuit@gmail.com?Subject=Book%20and%20Movie%20Club" target="_top">
+            awren.nuit@gmail.com
+          </a>
+          .
+        </p>
+        <p>
+          Click or tap on "Books", "Movies", or "Podcasts" to get goin'. Happy travels!~ &hearts;
+        </p>
       </div>
-      <div>
-        <Movies />
-      </div>
-      <div>
-        <Podcasts />
-      </div>
-    </>
+    </div>
   );
 }
