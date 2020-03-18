9d95e6290d13da5f7712f486baf9c764b021cca7
AwrenNuit <a.rennaker@yahoo.com>
2020-03-18 14:03:35 -0500

    made navigation sticky

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,28 +6,29 @@ export default function Nav() {
 
   return(
     <>
-    <nav>
-      <ul>
-        <Link to="/">
-          <li className="nav-name">Cozy Community Collective</li>
-        </Link>
-        <Link to="/">
-          <li>Home</li>
-        </Link>
-        <Link to="/books">
-          <li>Books</li>
-        </Link>
-        <Link to="/movies">
-          <li>Movies</li>
-        </Link>
-        <Link to="/podcasts">
-          <li>Podcasts</li>
-        </Link>
-        <Link to="/about">
-          <li>About</li>
-        </Link>
-      </ul>
-    </nav>
+      <nav>
+        <ul>
+          <Link to="/">
+            <li className="nav-name">Cozy Community Collective</li>
+          </Link>
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/books">
+            <li>Books</li>
+          </Link>
+          <Link to="/movies">
+            <li>Movies</li>
+          </Link>
+          <Link to="/podcasts">
+            <li>Podcasts</li>
+          </Link>
+          <Link to="/about">
+            <li>About</li>
+          </Link>
+        </ul>
+      </nav>
+      <div className="whitespace-bottom"></div>
     </>
   );
 }
